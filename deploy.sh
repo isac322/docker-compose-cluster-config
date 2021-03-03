a130242880edf6c09377a89668c58e561b080337
Byeonghoon Yoo <bh322yoo@gmail.com>
2021-03-03 23:38:13 +0900

    Support tls

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+STACK_NAME=bhyoo
+
 function deploy() {
   docker-compose -f "$1" config > /tmp/tmp.yml
-  docker stack deploy -c /tmp/tmp.yml "${1%.yml}"
+  docker stack deploy -c /tmp/tmp.yml "$STACK_NAME"
   rm /tmp/tmp.yml
 }
 
